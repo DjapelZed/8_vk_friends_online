dd2357b1af452f57a573630d769eadeb53209d2b
Maksim Kushnirov <maks.kushnirov@gmail.com>
2017-03-04 18:37:34 +0300

    Рабочий скрипт

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,49 @@
 import vk
+import time
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = None # Укажи ID своего Standalone-приложения
 
 
 def get_user_login():
-    pass
-
+    login = input('Введите свой логин: ')
+    if not login:
+        print('Ты не ввел логин!')
+        get_user_login()
+    return login
 
 def get_user_password():
-    pass
+    password = input('Введите свой пароль: ')
+    if not password:
+        print('Ты не ввел пароль!')
+        get_user_password()
+    return password
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends',
+        )
+    except:
+        print('Ошибка авторизации: неверный логин или пароль!')
+        get_online_friends(get_user_login(), get_user_password())
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline()
+    friends_online = []
+    for friend in friends_online_ids:
+        friends_online.append(api.users.get(user_ids=friend))
+        time.sleep(0.4)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Из друзей сейчас онлайн: ')
+    for friend in friends_online:
+        print('{0} {1}'.format(friend[0]['first_name'], friend[0]['last_name']))
 
 if __name__ == '__main__':
     login = get_user_login()
