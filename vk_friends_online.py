1089cceff89f3be12f2562d14ffee817243b018d
DjapelZed <maks.kushnirov@gmail.com>
2017-03-07 14:07:18 +0300

    Удалены костыли для запросов

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,5 +1,6 @@
 import vk
 import time
+import getpass
 
 
 APP_ID = None # Укажи ID своего Standalone-приложения
@@ -9,44 +10,45 @@ def get_user_login():
     login = input('Введите свой логин: ')
     if not login:
         print('Ты не ввел логин!')
-        get_user_login()
+        exit()
     return login
 
+
 def get_user_password():
-    password = input('Введите свой пароль: ')
+    print('* Пароль не будет отображаться в целях безопасности *')
+    password = getpass.getpass('Введите свой пароль:')
     if not password:
         print('Ты не ввел пароль!')
-        get_user_password()
+        exit()
     return password
 
 
 def get_online_friends(login, password):
-    try:
-        session = vk.AuthSession(
-            app_id=APP_ID,
-            user_login=login,
-            user_password=password,
-            scope='friends',
-        )
-    except:
-        print('Ошибка авторизации: неверный логин или пароль!')
-        get_online_friends(get_user_login(), get_user_password())
+    session = vk.AuthSession(
+        app_id=APP_ID,
+        user_login=login,
+        user_password=password,
+        scope='friends',
+    )
     api = vk.API(session)
     friends_online_ids = api.friends.getOnline()
-    friends_online = []
-    for friend in friends_online_ids:
-        friends_online.append(api.users.get(user_ids=friend))
-        time.sleep(0.4)
+    friends_online = api.users.get(user_ids=friends_online_ids)
     return friends_online
 
 
 def output_friends_to_console(friends_online):
     print('Из друзей сейчас онлайн: ')
     for friend in friends_online:
-        print('{0} {1}'.format(friend[0]['first_name'], friend[0]['last_name']))
+        print('{0} {1}'.format(friend['first_name'], friend['last_name']))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError as error:
+        print('Неверный логин или пароль!')
+        exit()
+
     output_friends_to_console(friends_online)
